bca9d33627fd9721c4bef235a48604f97853d082
ailtonbsj <ailton.ifce@gmail.com>
2024-03-21 11:41:11 -0300

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ddrescue-split-calculator';
+  chunks = '0';
+
+  deviceIn = '/dev/sdb';
+  deviceLen = '1000';
+  folderOut = '/mnt/device';
+  ansBackup = '# Nenhum';
+
+  folderIn = '/mnt/device';
+  deviceOut = '/dev/sdc';
+  ansRestore = '# Nenhum';
+
+  backup() {
+    if (this.chunks === '0') {
+      this.ansBackup = `sudo ddrescue -d -r3 ${this.deviceIn} ${this.folderOut}/bkp.img bkp.log`;
+    } else {
+      const deviceLen = parseInt(this.deviceLen);
+      const chuncks = parseInt(this.chunks);
+      let i = 0;
+      this.ansBackup = '';
+      for (let i = 0; i * chuncks < deviceLen; i++) {
+        this.ansBackup += `sudo ddrescue -d -r3 -o 0 -i ${i * chuncks} -s ${this.chunks} ${this.deviceIn} ${this.folderOut}/bkp${i}.img bkp${i}.log\n`;
+      }
+    }
+  }
+
+  restore() {
+    if (this.chunks === '0') {
+      this.ansRestore = `sudo ddrescue -d -r3 ${this.folderIn}/bkp.img ${this.deviceOut} rst.log`;
+    } else {
+      const deviceLen = parseInt(this.deviceLen);
+      const chuncks = parseInt(this.chunks);
+      let i = 0;
+      this.ansRestore = '';
+      for (let i = 0; i * chuncks < deviceLen; i++) {
+        this.ansRestore += `sudo ddrescue -d -r3 -o ${i * chuncks} ${this.folderIn}/bkp${i}.img ${this.deviceOut} rst${i}.log\n`;
+      }
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule} from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -10,7 +18,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatInputModule,
+    FormsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
